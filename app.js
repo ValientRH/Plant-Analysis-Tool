a78433c83f99ea8675695a9b5003ef518c85aa4f
ValientRH <shub32323@gmail.com>
2025-06-30 02:31:57 +0530

    added nojejs api key for gemini

--- app.js
+++ app.js
@@ -5,6 +5,29 @@ const pdfkit = require("pdfkit");
 const fs = require("fs");
 const fsPromises = fs.promises;
 const path = require("path");
-const {GoogleGenerativeAI}  = require("@google/genai");
+const {GoogleGenerativeAI}  = require("@google/generative-ai");
+
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
+
+//configure multer for file upload
+const upload = multer({dest: "upload/"});
+app.use(express.json({limit: "10mb"}));
+
+//initilize genai
+const genai = new GoogleGenerativeAI(process.env.GeminiApiKey);
+app.use(express.static("public"));
+
+//routes
+app.post('/analyze', async(req, res)=>{
+    res.json({ success: true });
+})
+app.post('/download', async(req, res)=>{
+    res.json({ success: true });
+})
+
+//start the serve
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
